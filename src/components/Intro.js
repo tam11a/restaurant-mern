99b1d521251972b653449c7cf2b763ccfea1b598
tam11a <ibrahimsadiktamim@gmail.com>
2021-07-18 05:56:22 +0600

    first commit

---
+++ src/components/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Intro.css';
+
+export default function Intro() {
+    return (
+        <div className="intro">
+            <div className="intro-dial">
+                <h1>Fine Dine</h1>
+                <p className="idp">LIFESTYLERS</p>
+                <p className="idp-1">This blog for people who care about healthy food</p>
+                <div className="hr"></div>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Nav.css';
+
+export default function Nav() {
+    return (
+        <div className="Nav flex-inline">
+            <ul className="flex-inline">
+                <li>home</li>
+                <li>featured</li>
+                <li>breakfast</li>
+                <li>fruits</li>
+                <li>contact</li>
+            </ul>
+            <div className="flex-inline">
+
+            </div>
+        </div>
+    )
+}
